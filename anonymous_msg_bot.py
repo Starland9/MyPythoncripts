48caa38f0b93827bc9a72f0a5447524fa6cb193c
Landry Simo <landrysimo99@gmail.com>
2024-11-09 14:37:01 +0100

    feat: Implement message storage and error handling
    Add functionality to store messages sent by the bot and handle potential errors. This includes creating files to store sent messages and errors, and adding logic to the main loop to write to these files. This enhances the bot's reliability and provides insights into its operation.

--- anonymous_msg_bot.py
+++ anonymous_msg_bot.py
@@ -7,44 +7,61 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import subprocess
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-link = "https://ngl.link/f.d20226/"
 
-messages = [];
+messages = []
+
 
 def get_ai_response():
     response = g4f.ChatCompletion.create(
-        model=g4f.models.Phi_3_mini_4k_instruct,
-        messages=[{"role": "user", "content": f"Donne moi une phrase de drague pour une fille, tu peux y mettre des emojis et beaucoup de personalisation , de romantisme et d'autenticité. ta phrase ne doit pas faire partir de cette liste de messages {messages} "}],
+        model=g4f.models.blackboxai,
+        messages=[
+            {
+                "role": "user",
+                "content": f"Donne moi une phrase de drague pour une fille, tu peux beaucoup de personalisation , de romantisme et d'autenticité. ta phrase ne doit pas faire partir de cette liste de messages {messages} et doit etre breve",
+            }
+        ],
     )
     return response
 
-while True:
-    try:
-        driver.get(link)
-        textarea = driver.find_element(By.TAG_NAME, "textarea")
-        textarea.click()
-        time.sleep(1)
-        msg = get_ai_response()
-        print(f"=> {msg}\n")
-        textarea.send_keys(msg)
-        button = driver.find_element(By.TAG_NAME, "button")
-        button.click()
-        messages.append(msg)
-        
-        with open("messages.txt", "w") as f:
-            for item in messages:
-                f.write("%s\n" % item)
 
-        time.sleep(1)
-    except Exception as e:
-        continue
-        
+def do_magic():
 
-            
-        
-    
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
 
-# if __name__ == "__main__":
-#     r = get_ai_response()
-#     print(r)
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=chrome_options
+    )
+    link = "https://ngl.link/arianenancy"
+
+    while True:
+        try:
+            driver.get(link)
+            textarea = driver.find_element(By.TAG_NAME, "textarea")
+            textarea.click()
+            time.sleep(1)
+            msg = get_ai_response()
+            msg = str(msg).split("\n")[-1].split('"')[1]
+            print(f"=> {msg}\n")
+            textarea.send_keys(msg)
+            button = driver.find_element(By.TAG_NAME, "button")
+            button.click()
+            messages.append(msg)
+
+            with open("messages.txt", "w") as f:
+                for item in messages:
+                    f.write("%s\n" % item)
+
+            time.sleep(1)
+        except Exception as e:
+            with open("error.txt", "w") as f:
+                f.write(str(e))
+            continue
+
+
+if __name__ == "__main__":
+    do_magic()
+
+    # msg = get_ai_response()
